a1e41676f177435883cba134ea1bee4c7b88e85c
Tamires Manolio Vanucci <tamiresvanucci.2@gmail.com>
2025-03-19 18:57:30 -0300

    feat: :sparkles: Removi os arquivos de app.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
   imports: [
@@ -12,17 +10,20 @@ import { AppService } from './app.service';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.POSTGRES_HOST || 'localhost',
-      port: parseInt(process.env.POSTGRES_PORT, 10) || 5432,
+      port: parseInt(process.env.POSTGRES_PORT as string, 10) || 5432,
       username: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DB,
-      entities: [],
+      entities: [], 
       synchronize: process.env.TYPEORM_SYNC === 'true', 
       logging: process.env.TYPEORM_LOGGING === 'true',
     })
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
+
+
+
 
